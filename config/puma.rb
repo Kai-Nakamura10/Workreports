0ee97df6988b13c18e2ec2425c72727af7120ee9
kainakamura <yamadeko2761@gmail.com>
2025-07-04 15:14:13 +0900

    #

--- config/puma.rb
+++ config/puma.rb
@@ -27,8 +27,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Render でのデフォルトポートに対応
+port ENV.fetch("PORT") { 10000 }
+
+# 明示的に 0.0.0.0 にバインド（重要）
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 10000 }}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
@@ -36,6 +39,5 @@ plugin :tmp_restart
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
+# Specify the PID file
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
