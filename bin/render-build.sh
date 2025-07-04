3ad4ff786cef088449f3948aa9e5c8ef08de32e5
kainakamura <yamadeko2761@gmail.com>
2025-07-04 16:04:05 +0900

    ER

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit
-
-bundle install
 bin/rails assets:precompile
-bin/rails assets:clean
 
 bin/rails db:migrate
+bundle exec rails db:migrate DATABASE=queue
+bundle exec rails db:migrate DATABASE=cache
+bundle exec rails db:migrate DATABASE=cable
