a07a55c28cb151c978c0a3a737f7e7f6703fef53
kainakamura <yamadeko2761@gmail.com>
2025-08-15 15:11:41 +0900

    メール送信機能の実装

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,6 +3,6 @@ class ReportMailer < ApplicationMailer
 
   def report_notification(report, to:, cc: nil, bcc: nil, subject:, body: nil)
     @report = report
-    mail(to: to, cc: cc, bcc: bcc, subject: subject)
+    mail(from: ENV.fetch('MAIL_FROM'), to: to, cc: cc, bcc: bcc, subject: subject)
   end
 end
